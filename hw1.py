7d9def406154d5a7322d626336b617e89aafda42
Kristina Kolibab <tinytina228@yahoo.com>
2017-09-19 22:30:50 -0400

    Update hw1.py
    Final Version

--- hw1.py
+++ hw1.py
@@ -15,6 +15,9 @@ def readTrainingFile( file ):
 		trainList.append(line.split(' ')[0:-2]) # read in each point 
 		trueAnswer.append(line.split(' ')[-2]) # read in each 'Y'/'N' answer
 	f.close()
+	trainList = np.array(trainList, dtype = float) # convert to numpy array
+	trainList = trainList/np.linalg.norm(trainList)
+	np.array(trueAnswer) 
 	return trueAnswer, trainList
 
 # Correctly reads in the training set based on it's formatting
@@ -23,74 +26,60 @@ def readTestFile( file ):
 	f = open(file, "r")
 	vecTesters = []
 	for line in f:
-		vecTesters.append(line)
+		vecTesters.append(line.split(' ')[0:-1])
+	f.close()
+	vecTesters = np.array(vecTesters, dtype = float)
+	vecTesters = vecTesters/np.linalg.norm(vecTesters)
 	return vecTesters
+
+# Converts my binary to ascii character
+def bin_ascii( str ):
+	int(str, 2)
+	str = chr(int(str, 2))
+	return str
 
 # Classifier function to figure out my weight 'w' 
 def classifier():
-
+	
         # Initial stuff that does not get repeated
-        trueAnswer = []
-        trainList = []
         trueAnswer, trainList = readTrainingFile("train.txt") # fetching vectors
-#        print "trainList: ", trainList
-        w = np.zeros((792, 1024)) # initially set weight to 0
-#        print "w: ", w
-        Error = True
+        w = np.zeros(1024) # initially set weight to 0, w should only be of 1024, a LINE
+	Error = True
         errorCounter = 0
+	i = 0
         # Repeats until Error = false, meaning the training set effectively trained w
-        while Error == True:
-            print "-----------------TIME THROUGH: ", errorCounter
-            errorCounter += 1
-            Error = False
-        
-#            guess = np.zeros((792, 1024))
-            guess = np.dot(w, trainList)
-            # compare trueAnswer(yHat) with guess(value)  
-            for (i, value) in enumerate(guess):
-                yHat = trueAnswer[i] # only one answer per row i
-                updateW = 0
-                if value >= 0: # y = +1, guess value is 1
-                    sign = 1
-                    if yHat != 'Y': # guess not truly above the line 
-                        updateW = 1
-                elif value < 0: # y = -1, guess value is 0
-                    sign = -1
-                    if yHat != 'N': # guess not truly below the line
-                        updateW = 0
-              
-                if updateW == 1:
-                    print "Incorrect Guess"
-                    print "w before: ", w
-                    Error = True
-                    np.add(np.dot(trainList, sign), w)
-                    print "w after: ", w
-                elif updateW == 0:
-                    print "Correct Guess"
-
-            # Done training, time to test
-            print "Weight has been correctly trained, now to test!"
-            vecTesters = []
-            vecTesters = readTestFile("test.txt")
-            ansVec = []
-            ansVec = np.dot(w, vecTesters)
-
-            for AnsValue in ansVec: 
-                if AnsValue >= 0:
-                    binary = 1
-                    print "binary: ", binary
-                elif AnsValue < 0:
-                    binary = 0
-                    print "binary: ", binary
-                binaryVec.append(binary) # 8 total
-        
-        # print ansVec    
-        # call a hex function, ascii char 
+        while i != len(trainList) :
+		Error = False
+            	errorCounter += 1 
+	   	for row in trainList: 
+                	if np.dot(w, row) >= 0 and trueAnswer[i] != 'Y': # y = +1, guess value is 1
+                		Error = True
+				i = 0
+				w = np.add(-1*row, w)
+				break
+                	elif np.dot(w, row) < 0 and trueAnswer[i] != 'N': # y = -1, guess value is 0
+	            		Error = True			
+				i = 0
+				w = np.add(1*row, w)
+				break
+			else: 
+				i += 1
+				
+	print "Iterations: ", errorCounter
+        # Done training, time to test
+	vecTesters = readTestFile("test.txt")		
+	binaryS = ''
+	for row in vecTesters:
+		if np.dot(w, row) >= 0:
+			binaryS = binaryS + '1'		
+		elif np.dot(w, row) < 0:
+			binaryS = binaryS + '0'
+	print "Binary: ", binaryS
+	print "ASCII Char: ", bin_ascii(binaryS)
 
 # M A I N
 def main():
-
-    classifier()
+	classifier()
 
 if __name__ == "__main__":
     main()
